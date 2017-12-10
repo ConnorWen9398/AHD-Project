108c5799e6cd4310da217fe1fd325514aadc3130
dsc7224 <31324593+dsc7224@users.noreply.github.com>
2017-12-10 18:00:01 -0500

    FPGA board in n out

---
+++ Project test/FPGA board_2/MUX.vhd
@@ -0,0 +1,46 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    11:41:57 11/29/2017 
+-- Design Name: 
+-- Module Name:    MUX - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity MUX is
+    Port ( in0 : in  STD_LOGIC_VECTOR (31 downto 0);
+           in1 : in  STD_LOGIC_VECTOR (31 downto 0);
+           sel : in  STD_LOGIC;
+           output : out  STD_LOGIC_VECTOR (31 downto 0));
+end MUX;
+
+architecture Behavioral of MUX is
+
+begin
+	output <= in1 when sel='1' else
+				 in0;
+
+end Behavioral;
+
